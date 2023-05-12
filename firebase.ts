8d6bc65843e95a391b9d39ca48d46346c35bdeb9
isobe <121940353+mayucoisobe@users.noreply.github.com>
2023-05-12 17:34:04 +0900

    firebase開始

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIPEazd2FZ8jN2Xn1Dki9LFoudrKuncRI',
+  authDomain: 'cine-chat.firebaseapp.com',
+  projectId: 'cine-chat',
+  storageBucket: 'cine-chat.appspot.com',
+  messagingSenderId: '206703456947',
+  appId: '1:206703456947:web:e9755f1f59001d75ea36c6',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
